12be52e429b3ca3c1b3562667c21d80033cf9528
Thomas Newton <thomas.w.newton@gmail.com>
2025-03-31 12:39:34 +0100

    Working when reading Azure

--- crates/deltalake/examples/read_delta_table.rs
+++ crates/deltalake/examples/read_delta_table.rs
@@ -1,7 +1,10 @@
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<(), deltalake::errors::DeltaTableError> {
+    deltalake::azure::register_handlers(None);
+
     let table_path = "abfss://databricks@wayveproddataset.dfs.core.windows.net/delta/inferred__metadata/corrected_standardised_runtime_vehicle_calibration";
-    let storage_options = std::collections::HashMap::from([("use_azure_cli", "true")]);
+    let storage_options =
+        std::collections::HashMap::from([(String::from("use_azure_cli"), String::from("true"))]);
     let table = deltalake::open_table_with_storage_options(table_path, storage_options).await?;
     println!("{table}");
     Ok(())
