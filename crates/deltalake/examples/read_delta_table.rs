5d7f778daa9528db68aa340f1ff3fbfd15a270ef
Thomas Newton <thomas.w.newton@gmail.com>
2025-03-31 14:30:20 +0100

    Run test in a loop

--- crates/deltalake/examples/read_delta_table.rs
+++ crates/deltalake/examples/read_delta_table.rs
@@ -1,3 +1,5 @@
+use deltalake::DeltaTableBuilder;
+
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<(), deltalake::errors::DeltaTableError> {
     deltalake::azure::register_handlers(None);
@@ -5,7 +7,18 @@ async fn main() -> Result<(), deltalake::errors::DeltaTableError> {
     let table_path = "abfss://databricks@wayveproddataset.dfs.core.windows.net/delta/inferred__metadata/corrected_standardised_runtime_vehicle_calibration";
     let storage_options =
         std::collections::HashMap::from([(String::from("use_azure_cli"), String::from("true"))]);
-    let table = deltalake::open_table_with_storage_options(table_path, storage_options).await?;
-    println!("{table}");
+
+    for i in 0..500 {
+        for version in 50480..50500 {
+            println!("i={i} version={version}");
+            let table = DeltaTableBuilder::from_valid_uri(table_path)?
+                .with_storage_options(storage_options.clone())
+                .with_version(version)
+                .load()
+                .await?;
+            println!("{table}");
+        }
+    }
+
     Ok(())
 }
